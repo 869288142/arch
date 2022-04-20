18f21a7105c2dd3596a21d0d12e849c8c73bbc64
869288142 <869288142@qq.com>
2022-04-20 23:03:54 +0800

    refactor(di): remove di demo

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,6 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import { createPinia } from "pinia";
-import "reflect-metadata";
-import { TYPES } from "./types";
-import { Warrior } from "./interfaces";
-import { myContainer } from "./entities";
-const ninja = myContainer.get<Warrior>(TYPES.Warrior);
-console.log(ninja.fight());
-console.log(ninja.sneak());
-
 createApp(App).use(createPinia()).mount("#app");
 
 
