79559032a648f72db1ed9e03aba54c84d923e71f
869288142 <869288142@qq.com>
2022-08-24 18:11:46 +0800

    feat(build): support http2 and proxy

--- src/request/index.ts
+++ src/request/index.ts
@@ -3,7 +3,8 @@ import axios from 'axios';
 const http = axios.create();
 
 
-http.defaults.baseURL = 'http://localhost:5000'
+// http.defaults.baseURL = 'http://localhost:5000'
+http.defaults.baseURL = '/api/'
 // http.interceptors.response.use(
 //     (response) => {
 //         const { d } = response.data;
--- vite.config.ts
+++ vite.config.ts
@@ -1,24 +1,49 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import path from 'path';
-import vueJsx from '@vitejs/plugin-vue-jsx'
-
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import path from "path";
+import vueJsx from "@vitejs/plugin-vue-jsx";
+import basicSsl from "@vitejs/plugin-basic-ssl";
+import proxy from "vite-plugin-http2-proxy";
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
+    https: true,
     fs: {
-      strict:false,
+      strict: false,
       // Allow serving files from one level up to the project root
       // allow: ['..']
-    }
+    },
+
+    // proxy: {
+    //   "/api": {
+    //     // jm.ajax使用
+    //     target: `http://localhost:5000/`,
+    //     changeOrigin: true,
+    //     secure: false,
+    //     rewrite: (path) => path.replace(/^\/api/, ""),
+    //   },
+    // },
   },
   resolve: {
     alias: {
-      "@":  path.resolve(__dirname, "src")
-    }
+      "@": path.resolve(__dirname, "src"),
+    },
   },
 
-  plugins: [vueJsx(),vue({
-    reactivityTransform: true
-  })]
-})
+  plugins: [
+    vueJsx(),
+    vue({
+      reactivityTransform: true,
+    }),
+    basicSsl(),
+    proxy({
+      "/api": {
+        // jm.ajax使用
+        target: `http://localhost:5000/`,
+        // changeOrigin: true,
+        secure: false,
+        rewrite: (path) => path.replace(/^\/api/, ""),
+      },
+    }),
+  ],
+});
