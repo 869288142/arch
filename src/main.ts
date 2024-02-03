2f7127eb29cc6ce8c0d2fe256016565b42e97316
869288142 <869288142@qq.com>
2024-02-03 14:55:27 +0800

    chore: remove element-plus

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,4 @@
 import { createApp } from "vue";
-import ElementPlus from 'element-plus'
-import 'element-plus/dist/index.css'
 import App from "./App.vue";
 import { createPinia } from "pinia";
 import { goodsServiceToken } from "./shared/contants/token";
